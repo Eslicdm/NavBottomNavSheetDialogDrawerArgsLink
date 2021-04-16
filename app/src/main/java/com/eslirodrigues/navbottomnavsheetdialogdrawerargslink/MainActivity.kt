948dde02028ae892846a9f46c0677e6ce801b325
Eslicdm <eslicdm1@gmail.com>
2021-04-16 08:55:30 -0300

    Drawer Layout completed

--- app/src/main/java/com/eslirodrigues/navbottomnavsheetdialogdrawerargslink/MainActivity.kt
+++ app/src/main/java/com/eslirodrigues/navbottomnavsheetdialogdrawerargslink/MainActivity.kt
@@ -2,10 +2,35 @@ package com.eslirodrigues.navbottomnavsheetdialogdrawerargslink
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.eslirodrigues.navbottomnavsheetdialogdrawerargslink.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        navController = findNavController(R.id.fragmentHost)
+
+        binding.drawerView.setupWithNavController(navController)
+
+        appBarConfiguration = AppBarConfiguration(navController.graph, binding.drawerLayout)
+
+        setupActionBarWithNavController(navController, appBarConfiguration)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
 }
